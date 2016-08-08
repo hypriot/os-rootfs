8ba4e238e9a6c4fb012a5b75bcd16b4e3ae2bf21
Dieter Reuter <dieter.reuter@me.com>
2016-08-08 12:54:33 +0200

    Fix typo in comment

--- builder/build.sh
+++ builder/build.sh
@@ -32,7 +32,7 @@ if [[ "${VARIANT}" = "raspbian" ]]; then
   DEBOOTSTRAP_URL="http://mirrordirector.raspbian.org/raspbian/"
   DEBOOTSTRAP_KEYRING_OPTION="--keyring=/etc/apt/trusted.gpg"
 
-  # for Rasbian we need an extra gpg key to be able to access the repository
+  # for Raspbian we need an extra gpg key to be able to access the repository
   mkdir -p /builder/files/tmp
   wget -v -O "/builder/files/tmp/raspbian.public.key" http://mirrordirector.raspbian.org/raspbian.public.key
   get_gpg A0DA38D0D76E8B5D638872819165938D90FDDD2E "/builder/files/tmp/raspbian.public.key"
